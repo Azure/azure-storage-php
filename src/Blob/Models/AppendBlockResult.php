6b02d1fbd1c49115b700a9ce69f975eab313078e
hasonmsft <hason@microsoft.com>
2017-03-22 16:16:44 +0800

    Update the year to 2017 in copyright header

--- src/Blob/Models/AppendBlockResult.php
+++ src/Blob/Models/AppendBlockResult.php
@@ -17,7 +17,7 @@
  * @category  Microsoft
  * @package   MicrosoftAzure\Storage\Blob\Models
  * @author    Azure Storage PHP SDK <dmsh@microsoft.com>
- * @copyright 2016 Microsoft Corporation
+ * @copyright 2017 Microsoft Corporation
  * @license   https://github.com/azure/azure-storage-php/LICENSE
  * @link      https://github.com/azure/azure-storage-php
  */
