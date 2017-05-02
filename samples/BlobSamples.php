239e9fc4e50dca2822d7808efe0f972178c676a5
Tank Tang <kat@microsoft.com>
2017-05-02 10:59:29 +0800

    Adding samples to cover external sample cases, and refine the `BreakingChanges.md` and `Changelog.md`

--- samples/BlobSamples.php
+++ samples/BlobSamples.php
@@ -27,15 +27,40 @@ require_once "../vendor/autoload.php";
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 use MicrosoftAzure\Storage\Blob\Models\ListContainersResult;
+use MicrosoftAzure\Storage\Blob\Models\DeleteBlobOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateBlobOptions;
+use MicrosoftAzure\Storage\Blob\Models\GetBlobOptions;
+use MicrosoftAzure\Storage\Blob\Models\ContainerAcl;
+use MicrosoftAzure\Storage\Blob\Models\SetBlobPropertiesOptions;
+use MicrosoftAzure\Storage\Blob\Models\PageRange;
+use MicrosoftAzure\Storage\Blob\Models\ListPageBlobRangesOptions;
 use MicrosoftAzure\Storage\Common\ServicesBuilder;
+use MicrosoftAzure\Storage\Common\Models\Logging;
+use MicrosoftAzure\Storage\Common\Models\Metrics;
+use MicrosoftAzure\Storage\Common\Models\RetentionPolicy;
+use MicrosoftAzure\Storage\Common\Models\ServiceProperties;
 use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
 use MicrosoftAzure\Storage\Common\Exceptions\InvalidArgumentTypeException;
 
 $connectionString = 'DefaultEndpointsProtocol=https;AccountName=<yourAccount>;AccountKey=<yourKey>';
+$connectionString = 'DefaultEndpointsProtocol=https;AccountName=phput;AccountKey=LCtFmRVvbWj9C+U2GPF4oLXWZRABJgRn8pS6hBxeoFYDQNPk/2teTxqAJO8GmYHoNPKU+bIWtBxTN42BorMlXQ==';
+
 $blobClient = ServicesBuilder::getInstance()->createBlobService($connectionString);
+
+// Get and Set Blob Service Properties
+blobServiceProperties($blobClient);
 
 // To create a container call createContainer.
 createContainerSample($blobClient);
+
+// To get/set container properties
+containerProperties($blobClient);
+
+// To get/set container metadata
+containerMetadata($blobClient);
+
+// To get/set container ACL
+containerAcl($blobClient);
 
 // To upload a file as a blob, use the BlobRestProxy->createBlockBlob method. This operation will
 // create the blob if it doesn't exist, or overwrite it if it does. The code example below assumes 
@@ -50,6 +75,21 @@ downloadBlobSample($blobClient);
 // through the result. The following code outputs the name and URI of each blob in a container.
 listBlobsSample($blobClient);
 
+// To get set blob properties
+blobProperties($blobClient);
+
+// To get set blob metadata
+blobMetadata($blobClient);
+
+// Basic operations for page blob.
+pageBlobOperations($blobClient);
+
+// Snap shot operation for blob service.
+snapshotOperations($blobClient);
+
+// Basic lease operations.
+leaseOperations($blobClient);
+
 //Or to leverage the asynchronous methods provided, the operation can be done in
 //a promise pipeline.
 $containerName = '';
@@ -63,9 +103,6 @@ try {
     echo $e->getMessage().PHP_EOL;
 }
 
-
-//delete the containers created. Uncomment the following code for the task.
-/*
 try {
     cleanUp($blobClient, $containerName)->wait();
 } catch (ServiceException $e) {
@@ -73,7 +110,40 @@ try {
     $error_message = $e->getMessage();
     echo $code.": ".$error_message.PHP_EOL;
 }
-*/
+
+function blobServiceProperties($blobClient)
+{
+     // Get blob service properties
+    echo "Get Blob Service properties" . PHP_EOL;
+    $originalProperties = $blobClient->getServiceProperties();
+    // Set blob service properties
+    echo "Set Blob Service properties" . PHP_EOL;
+    $retentionPolicy = new RetentionPolicy();
+    $retentionPolicy->setEnabled(true);
+    $retentionPolicy->setDays(10);
+    
+    $logging = new Logging();
+    $logging->setRetentionPolicy($retentionPolicy);
+    $logging->setVersion('1.0');
+    $logging->setDelete(true);
+    $logging->setRead(true);
+    $logging->setWrite(true);
+    
+    $metrics = new Metrics();
+    $metrics->setRetentionPolicy($retentionPolicy);
+    $metrics->setVersion('1.0');
+    $metrics->setEnabled(true);
+    $metrics->setIncludeAPIs(true);
+    $serviceProperties = new ServiceProperties();
+    $serviceProperties->setLogging($logging);
+    $serviceProperties->setHourMetrics($metrics);
+    $blobClient->setServiceProperties($serviceProperties);
+    
+    // revert back to original properties
+    echo "Revert back to original service properties" . PHP_EOL;
+    $blobClient->setServiceProperties($originalProperties->getValue());
+    echo "Service properties sample completed" . PHP_EOL;
+}
 
 function createContainerSample($blobClient)
 {
@@ -100,6 +170,154 @@ function createContainerSample($blobClient)
         $error_message = $e->getMessage();
         echo $code.": ".$error_message.PHP_EOL;
     }
+}
+
+function containerProperties($blobClient)
+{
+    $containerName = "mycontainer" . generateRandomString();
+    
+    echo "Create container " . $containerName . PHP_EOL;
+    // Create container options object.
+    $createContainerOptions = new CreateContainerOptions();
+    // Set public access policy. Possible values are
+    // PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
+    // CONTAINER_AND_BLOBS: full public read access for container and blob data.
+    // BLOBS_ONLY: public read access for blobs. Container data not available.
+    // If this value is not specified, container data is private to the account owner.
+    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+    // Set container metadata
+    $createContainerOptions->addMetaData("key1", "value1");
+    $createContainerOptions->addMetaData("key2", "value2");
+    // Create container.
+    $blobClient->createContainer($containerName, $createContainerOptions);
+    echo "Get container properties:" . PHP_EOL;
+    // Get container properties
+    $properties = $blobClient->getContainerProperties($containerName);
+    echo 'Last modified: ' . $properties->getLastModified()->format('Y-m-d H:i:s') . PHP_EOL;
+    echo 'ETAG: ' . $properties->getETag() . PHP_EOL;
+    echo "Delete container" . PHP_EOL;
+    $blobClient->deleteContainer($containerName) . PHP_EOL;
+}
+
+function containerMetadata($blobClient)
+{
+    $containerName = "mycontainer" . generateRandomString();
+    
+    echo "Create container " . $containerName . PHP_EOL;
+    // Create container options object.
+    $createContainerOptions = new CreateContainerOptions();
+    // Set container metadata
+    $createContainerOptions->addMetaData("key1", "value1");
+    $createContainerOptions->addMetaData("key2", "value2");
+    // Create container.
+    $blobClient->createContainer($containerName, $createContainerOptions);
+    echo "Get container metadata" . PHP_EOL;
+    // Get container properties
+    $properties = $blobClient->getContainerProperties($containerName);
+    foreach ($properties->getMetadata() as $key => $value) {
+        echo $key . ": " . $value . PHP_EOL;
+    }
+    echo "Delete container" . PHP_EOL;
+    $blobClient->deleteContainer($containerName);
+}
+
+function containerAcl($blobClient)
+{
+    // Create container
+    $container = "mycontainer" . generateRandomString();
+    echo "Create container " . $container . PHP_EOL;
+    $blobClient->createContainer($container);
+    // Set container ACL
+    $past = new \DateTime("01/01/2010");
+    $future = new \DateTime("01/01/2020");
+    $acl = new ContainerAcl();
+    $acl->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+    $acl->addSignedIdentifier('123', $past, $future, 'rw');
+    $blobClient->setContainerACL($container, $acl);
+    // Get container ACL
+    echo "Get container access policy" . PHP_EOL;
+    $acl = $blobClient->getContainerACL($container);
+    echo 'Public access: ' . $acl->getContainerACL()->getPublicAccess() . PHP_EOL;
+    echo 'Signed Identifiers: ' . PHP_EOL;
+    echo ' Id: ' . $acl->getContainerACL()->getSignedIdentifiers()[0]->getId() . PHP_EOL;
+    echo ' Start: '. $acl->getContainerACL()->getSignedIdentifiers()[0]->getAccessPolicy()->getStart()->format('Y-m-d H:i:s') .PHP_EOL;
+    echo ' Expiry: '. $acl->getContainerACL()->getSignedIdentifiers()[0]->getAccessPolicy()->getExpiry()->format('Y-m-d H:i:s') .PHP_EOL;
+    echo ' Permission: '. $acl->getContainerACL()->getSignedIdentifiers()[0]->getAccessPolicy()->getPermission() .PHP_EOL;
+    echo "Delete container" . PHP_EOL;
+    $blobClient->deleteContainer($container);
+}
+
+function blobProperties($blobClient)
+{
+    // Create container
+    $container = "mycontainer" . generateRandomString();
+    echo "Create container " . $container . PHP_EOL;
+    $blobClient->createContainer($container);
+    // Create blob
+    $blob = 'blob' . generateRandomString();
+    echo "Create blob " . PHP_EOL;
+    $blobClient->createPageBlob($container, $blob, 4096);
+    // Set blob properties
+    echo "Set blob properties" . PHP_EOL;
+    $opts = new SetBlobPropertiesOptions();
+    $opts->setCacheControl('test');
+    $opts->setContentEncoding('UTF-8');
+    $opts->setContentLanguage('en-us');
+    $opts->setContentLength(512);
+    $opts->setContentMD5(null);
+    $opts->setContentType('text/plain');
+    $opts->setSequenceNumberAction('increment');
+    $blobClient->setBlobProperties($container, $blob, $opts);
+    // Get blob properties
+    echo "Get blob properties" . PHP_EOL;
+    $result = $blobClient->getBlobProperties($container, $blob);
+   
+    $props = $result->getProperties();
+    echo 'Cache control: ' . $props->getCacheControl() . PHP_EOL;
+    echo 'Content encoding: ' . $props->getContentEncoding() . PHP_EOL;
+    echo 'Content language: ' . $props->getContentLanguage() . PHP_EOL;
+    echo 'Content type: ' . $props->getContentType() . PHP_EOL;
+    echo 'Content length: ' . $props->getContentLength() . PHP_EOL;
+    echo 'Content MD5: ' . $props->getContentMD5() . PHP_EOL;
+    echo 'Last modified: ' . $props->getLastModified()->format('Y-m-d H:i:s') . PHP_EOL;
+    echo 'Blob type: ' . $props->getBlobType() . PHP_EOL;
+    echo 'Lease status: ' . $props->getLeaseStatus() . PHP_EOL;
+    echo 'Sequence number: ' . $props->getSequenceNumber() . PHP_EOL;
+    echo "Delete blob" . PHP_EOL;
+    $blobClient->deleteBlob($container, $blob);
+    echo "Delete container" . PHP_EOL;
+    $blobClient->deleteContainer($container);
+}
+
+function blobMetadata($blobClient)
+{
+    // Create container
+    $container = "mycontainer" . generateRandomString();
+    echo "Create container " . $container . PHP_EOL;
+    $blobClient->createContainer($container);
+    // Create blob
+    $blob = 'blob' . generateRandomString();
+    echo "Create blob " . PHP_EOL;
+    $blobClient->createPageBlob($container, $blob, 4096);
+    // Set blob metadata
+    echo "Set blob metadata" . PHP_EOL;
+    $metadata = array(
+        'key' => 'value',
+        'foo' => 'bar',
+        'baz' => 'boo');
+    $blobClient->setBlobMetadata($container, $blob, $metadata);
+    // Get blob metadata
+    echo "Get blob metadata" . PHP_EOL;
+    $result = $blobClient->getBlobMetadata($container, $blob);
+    
+    $retMetadata = $result->getMetadata();
+    foreach ($retMetadata as $key => $value) {
+        echo $key . ': ' . $value . PHP_EOL;
+    }
+    echo "Delete blob" . PHP_EOL;
+    $blobClient->deleteBlob($container, $blob);
+    echo "Delete container" . PHP_EOL;
+    $blobClient->deleteContainer($container);
 }
 
 function uploadBlobSample($blobClient)
@@ -215,6 +433,79 @@ function basicStorageBlobOperationAsync($blobClient)
     );
 }
 
+function pageBlobOperations($blobClient)
+{
+    $blobName = "HelloPageBlobWorld";
+    $containerName = 'mycontainer';
+      
+    # Create a page blob
+    echo "Create Page Blob with name {$blobName}".PHP_EOL;
+    $blobClient->createPageBlob($containerName, $blobName, 2560);
+    # Create pages in a page blob
+    echo "Create pages in a page blob".PHP_EOL;
+    
+    $blobClient->createBlobPages(
+        $containerName,
+        $blobName,
+        new PageRange(0, 511),
+        generateRandomString(512)
+    );
+    $blobClient->createBlobPages(
+        $containerName,
+        $blobName,
+        new PageRange(512, 1023),
+        generateRandomString(512)
+    );
+    # List page blob ranges
+    $listPageBlobRangesOptions = new ListPageBlobRangesOptions();
+    $listPageBlobRangesOptions->setRangeStart(0);
+    $listPageBlobRangesOptions->setRangeEnd(1023);
+    echo "List Page Blob Ranges".PHP_EOL;
+    $listPageBlobRangesResult = $blobClient->listPageBlobRanges(
+        $containerName,
+        $blobName,
+        $listPageBlobRangesOptions
+    );
+    
+    foreach ($listPageBlobRangesResult->getPageRanges() as $pageRange) {
+        echo "Range:".$pageRange->getStart()."-".$pageRange->getEnd().PHP_EOL;
+        $getBlobOptions = new GetBlobOptions();
+        $getBlobOptions->setRangeStart($pageRange->getStart());
+        $getBlobOptions->setRangeEnd($pageRange->getEnd());
+        $getBlobResult = $blobClient->getBlob($containerName, $blobName, $getBlobOptions);
+        file_put_contents("PageContent.txt", $getBlobResult->getContentStream());
+    }
+    # Clean up after the sample
+      echo "Delete Blob".PHP_EOL;
+    $blobClient->deleteBlob($containerName, $blobName);
+}
+
+function snapshotOperations($blobClient)
+{
+    $blobName = "HelloWorld";
+    $containerName = 'mycontainer';
+
+    # Upload file as a block blob
+    echo "Uploading BlockBlob".PHP_EOL;
+      
+    $content = 'test content hello hello world';
+    $blobClient->createBlockBlob($containerName, $blobName, $content);
+    # Create a snapshot
+    echo "Create a Snapshot".PHP_EOL;
+    $snapshotResult = $blobClient->createBlobSnapshot($containerName, $blobName);
+    # Retrieve snapshot
+    echo "Retrieve Snapshot".PHP_EOL;
+    $getBlobOptions = new GetBlobOptions();
+    $getBlobOptions->setSnapshot($snapshotResult->getSnapshot());
+    $getBlobResult = $blobClient->getBlob($containerName, $blobName, $getBlobOptions);
+    file_put_contents("HelloWorldSnapshotCopy.png", $getBlobResult->getContentStream());
+    # Clean up after the sample
+    echo "Delete Blob and snapshot".PHP_EOL;
+    $deleteBlobOptions = new DeleteBlobOptions();
+    $deleteBlobOptions->setDeleteSnaphotsOnly(false);
+    $blobClient->deleteBlob($containerName, $blobName, $deleteBlobOptions);
+}
+
 function cleanUp($blobClient, $containerName)
 {
     return $blobClient->listContainersAsync()->then(
@@ -237,4 +528,44 @@ function cleanUp($blobClient, $containerName)
         },
         null
     );
+}
+
+function leaseOperations($blobClient)
+{
+    // Create container
+    $container = "mycontainer" . generateRandomString();
+    echo "Create container " . $container . PHP_EOL;
+    $blobClient->createContainer($container);
+    // Create Blob
+    $blob = 'Blob' . generateRandomString();
+    echo "Create blob " . $blob . PHP_EOL;
+    $contentType = 'text/plain; charset=UTF-8';
+    $options = new CreateBlobOptions();
+    $options->setContentType($contentType);
+    $blobClient->createBlockBlob($container, $blob, 'Hello world', $options);
+    
+    // Acquire lease
+    $result = $blobClient->acquireLease($container, $blob);
+    try {
+        echo "Try delete blob without lease" . PHP_EOL;
+        $blobClient->deleteBlob($container, $blob);
+    } catch (ServiceException $e) {
+        echo "Delete blob with lease" . PHP_EOL;
+        $blobOptions = new DeleteBlobOptions();
+        $blobOptions->setLeaseId($result->getLeaseId());
+        $blobClient->deleteBlob($container, $blob, $blobOptions);
+    }
+    echo "Delete container" . PHP_EOL;
+    $blobClient->deleteContainer($container);
+}
+
+function generateRandomString($length = 6)
+{
+    $characters = 'abcdefghijklmnopqrstuvwxyz';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
 }
