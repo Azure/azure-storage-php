1a7521f0d3eabdc54361d37f12a39da1fb8647cd
Michal Špaček <mail@michalspacek.cz>
2019-03-22 02:24:03 +0100

    Use aliased TableModels

--- azure-storage-table/src/Table/Internal/ITable.php
+++ azure-storage-table/src/Table/Internal/ITable.php
@@ -255,7 +255,7 @@ interface ITable
      *
      * @param string                                                   $table   The name of
      * the table.
-     * @param Models\QueryEntitiesOptions|string|Models\Filters\Filter $options Coule be
+     * @param TableModels\QueryEntitiesOptions|string|Models\Filters\Filter $options Coule be
      * optional parameters, query string or filter to apply.
      *
      * @return \GuzzleHttp\Promise\PromiseInterface
