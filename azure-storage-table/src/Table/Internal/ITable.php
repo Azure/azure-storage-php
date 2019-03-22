4fa8ce99b42cdeb3fcad9dbfa71295b876508254
Michal Špaček <mail@michalspacek.cz>
2019-03-22 02:19:01 +0100

    Import GetServiceStatsResult not GetServiceStats

--- azure-storage-table/src/Table/Internal/ITable.php
+++ azure-storage-table/src/Table/Internal/ITable.php
@@ -27,7 +27,7 @@ namespace MicrosoftAzure\Storage\Table\Internal;
 use MicrosoftAzure\Storage\Table\Models as TableModels;
 use MicrosoftAzure\Storage\Common\Models\ServiceProperties;
 use MicrosoftAzure\Storage\Common\Models\ServiceOptions;
-use MicrosoftAzure\Storage\Common\Models\GetServiceStats;
+use MicrosoftAzure\Storage\Common\Models\GetServiceStatsResult;
 
 /**
  * This interface has all REST APIs provided by Windows Azure for Table service.
